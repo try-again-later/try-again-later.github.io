0d17a30907fa06e691b06dab50c7e9d6abcaf001
try-again-later <aa.popok@yandex.ru>
2022-08-09 08:01:35 +0300

    Make segments round

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -27,7 +27,7 @@ defineProps<{
     <div aria-hidden="true" class="flex gap-1 justify-center sm:justify-start">
       <div
         v-for="i in MAX_SKILL_LEVEL"
-        class="w-10 h-1"
+        class="w-10 h-1 rounded-full"
         :class="{ 'bg-orange-500': i <= skill.level, 'bg-gray-300': i > skill.level }"
       ></div>
     </div>
